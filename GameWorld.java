d4ee778644238303af70577709022c482c0cc498
brandere <128709448+brandere@users.noreply.github.com>
2023-06-09 09:16:26 -0400

    back ground

--- GameWorld.java
+++ GameWorld.java
@@ -16,6 +16,6 @@ public class GameWorld extends World
     public GameWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(900, 700, 1); 
+        super(700, 800, 1); 
     }
 }
