1cea60b8525eeeebec807e659ec1ca8bcd53e85d
KyaBetke <128709365+KyaBetke@users.noreply.github.com>
2023-06-12 19:54:04 -0400

    Player Jump

--- GameWorld.java
+++ GameWorld.java
@@ -15,7 +15,8 @@ public class GameWorld extends World
     public GameWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(700, 800, 1); 
+        super(700, 700, 1); 
         addObject(new Player(), 500, 300);
+        addObject(new Plank(), 500, 500);
     }
 }
--- Player.java
+++ Player.java
@@ -1,45 +1,57 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-/**
- * Write a description of class Player here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+import greenfoot.*;
 public class Player extends Actor
 {
-    private static final int GRAVITY = 1;
-    private int vSpeed;
-    private int hSpeed;
+    
+    private int speed;
+    private int gravity;
+    
     /**
      * Act - do whatever the Player wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
+        speed = 5;
         getInputs();
+        gravity--;
+       setLocation(getX(), getY() - gravity);
+       checkForJump();
+       if (isTouching(Plank.class)){
+           setLocation(getX(),getY() + 1);
+       }
         //gravity();
     }
     public void getInputs()
     {
-        if (Greenfoot.isKeyDown("up")){
+        
+        if (Greenfoot.isKeyDown("up") || Greenfoot.isKeyDown("w")){
             setLocation(getX(), getY() - 3);
+           
         }
-        if (Greenfoot.isKeyDown("down")){
+        
+        if (Greenfoot.isKeyDown("down") || Greenfoot.isKeyDown("s")){
             setLocation(getX(), getY() + 3);
+       
         }
-        if (Greenfoot.isKeyDown("left")){
+       
+        if (Greenfoot.isKeyDown("left") || Greenfoot.isKeyDown("a")){
             setLocation(getX() - 3, getY());
+
         }
-        if (Greenfoot.isKeyDown("right")){
+       
+        if (Greenfoot.isKeyDown("right") || Greenfoot.isKeyDown("d")){
             setLocation(getX() + 3, getY());
         }
     }
-    public void gravity()
+    
+    private void checkForJump()
     {
-        vSpeed += GRAVITY;
-        hSpeed += GRAVITY;
-        setLocation(getX(), getY()+vSpeed);
-        setLocation(getX(), getY()+hSpeed);
+        
+        Actor a = getOneIntersectingObject(Plank.class);
+        if (isTouching(Plank.class) && Greenfoot.isKeyDown("space")){
+             gravity = 20; // this will make the character jump
+        }
     }
 }
+
+
