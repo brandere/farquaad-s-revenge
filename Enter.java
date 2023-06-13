031f2d7dc0a8a0095a6daada063d44163cdc3cf6
brandere <128709448+brandere@users.noreply.github.com>
2023-06-13 08:29:42 -0400

    going to game world

---
+++ Enter.java
@@ -0,0 +1,26 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Enter here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Enter extends Actor
+{
+    /**
+     * Act - do whatever the Enter wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+         GreenfootImage img = new GreenfootImage (900,100);//creates image
+       //img.setColor(new Color(0,0,0));//sets color
+       //img.fill();//Fills image
+       
+       img.setColor(new Color (255,255,255));
+       img.setFont(new Font("Times New Roman", true, false, 35)); //Name , bold(T,F),italic(t,f),size
+       img.drawString("press enter to play the game",5,50);
+       setImage(img);
+    }
+}
--- SplashScreen.java
+++ SplashScreen.java
@@ -17,6 +17,11 @@ public class SplashScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(700, 800, 1); 
-        addObject(new Instructions(), 0, 0);
+        
+    }
+    public void act(){
+        if (Greenfoot.isKeyDown("backspace")){
+            Greenfoot.setWorld(new StartScreen());
+        }
     }
 }
