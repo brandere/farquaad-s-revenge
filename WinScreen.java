472865ba05d887ed3ac55377a77b1b8504b5418b
brandere <128709448+brandere@users.noreply.github.com>
2023-06-13 09:24:55 -0400

    worlds

--- WinScreen.java
+++ WinScreen.java
@@ -17,5 +17,6 @@ public class WinScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(700, 800, 1); 
+        addObject(new WIN(),353,398);
     }
 }
