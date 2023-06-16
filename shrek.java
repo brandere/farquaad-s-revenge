b2597f784e738ec4c4d957d2de23f6c3150f40fa
brandere <128709448+brandere@users.noreply.github.com>
2023-06-16 08:47:33 -0400

    animation

--- shrek.java
+++ shrek.java
@@ -12,6 +12,7 @@ public class Shrek extends Actor
     int aniTimer;
     GreenfootImage s1 = new GreenfootImage("shrek1.png");
     GreenfootImage s2 = new GreenfootImage("shrek2.png");
+    GreenfootImage s3 = new GreenfootImage("shrek3.png");
     /**
      * Act - do whatever the shrek wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -31,10 +32,11 @@ public class Shrek extends Actor
     public void animate()
     {
         //To not animate too quickly, only animate every 6 act cycles - alter this if your images are switching too quickly
-        if (aniTimer % 6 == 0){
+        if (aniTimer % 20 == 0){
             //Set image based on current aniCounter value
-            if (aniCounter == 1){setImage(s2);}
-            else if (aniCounter == 2){setImage(s1);}
+            if (aniCounter == 1){setImage(s1);}
+            else if (aniCounter == 2){setImage(s3);}
+            else if (aniCounter == 3){setImage(s2);}
             
 
             //Increment aniCounter - if it is 5, set it back to 1
